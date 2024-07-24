22e21058991fbed0abc47dc7952cdcd13ee03553
Bojan Sejnjanovic <bojan.sejnjanovic@gmail.com>
2024-07-24 08:04:36 +0000

    qlearning first try

--- RPS.py
+++ RPS.py
@@ -1,10 +1,41 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import pandas as pd
+import numpy as np
 
-def player(prev_play, opponent_history=[]):
+q_table = pd.DataFrame(np.zeros((3, 3)), columns=['R', 'P', 'S'], index=['R', 'P', 'S'])
+
+def player(prev_play, opponent_history=[], q_table=q_table, actions=[], n=[0]):
     opponent_history.append(prev_play)
+    n[0] += 1
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if not opponent_history[-1] or not opponent_history[-2]:
+        actions.append('R')
+        return 'R'
 
-    return guess
+    learning_rate = 0.7
+    gamma = 0.3
+    max_e = 1.0
+    min_e = 0.01
+    decay_rate = 0.005
+    epsilon = min_e + (max_e - min_e) * np.exp(-decay_rate * n[0])
+    
+    state = opponent_history[-2]
+    next_state = opponent_history[-1]
+    action = actions[-1]
+    if action == next_state:
+        reward = 0
+    elif action == 'R' and next_state == 'S' or action == 'P' and next_state == 'R' or action == 'S' and next_state == 'P':
+        reward = 1
+    else:
+        reward = -1
+
+    q_table.loc[state, action] = q_table.loc[state, action] + learning_rate * (reward + gamma * q_table.loc[next_state].max() - q_table.loc[state, action])
+
+    random_n = np.random.rand()
+    if random_n > epsilon:
+        next_action = q_table.columns[q_table[next_state].argmax()]
+    else:
+        next_action = q_table.columns[np.random.randint(3)]
+
+    actions.append(next_action)
+    return next_action
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
-from RPS import player
+from RPS import player, q_table
 from unittest import main
 
-play(player, quincy, 1000)
+# play(player, quincy, 1000)
 play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
-
+# play(player, kris, 1000)
+# play(player, mrugesh, 1000)
+print(q_table)
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
 
